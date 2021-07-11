cfe177f8c745335899b5e8378337bef11b63264d
Shree-git <sridhar.pandey1@gmail.com>
2021-07-11 09:34:22 -0500

    Change chat migration

--- db/migrate/20210711142434_create_chats.rb
+++ db/migrate/20210711142434_create_chats.rb
@@ -2,8 +2,8 @@ class CreateChats < ActiveRecord::Migration[6.1]
   def change
     create_table :chats do |t|
       t.text :message
-      t.string :sender
-      t.string :receiver
+      t.references :sender, foreign_key: { to_table: 'users' }
+      t.references :receiver, foreign_key: { to_table: 'users' }
 
       t.timestamps
     end
