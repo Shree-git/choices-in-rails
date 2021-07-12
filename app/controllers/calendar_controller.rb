92c2416af645adafbba30b067381825a0c5ab24d
Shree-git <sridhar.pandey1@gmail.com>
2021-07-11 22:44:22 -0500

    WIP Calendar Controller

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -4,5 +4,13 @@ class CalendarController < ApplicationController
     start_date = params.fetch(:start_date, Date.today).to_date
     @meetings = Meeting.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
+
+  def new
+    @meeting = Meeting.new
+  end
+
+  def create_event
+    # @meeting = Meeting.new()
+  end
   
 end
